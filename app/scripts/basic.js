adb3d90299d94339f219c5b80184d3cb90eb3920
Emily Wu <embrilliant@hotmail.com>
2016-07-08 19:35:05 +0200

    modify code

--- app/scripts/basic.js
+++ app/scripts/basic.js
@@ -1,11 +1,7 @@
 
-// 1. use single class, e.g. js-block v
-// 2. use classList v
-// 3. move DOM elements into variables outside of scroll callback v
-// 4. move menuClass outside of scroll callback v
-// 5. remove magic numbers v
-// 6. set windowHeight v
-// 7. loop through each DOM element and if its offsetTop is between topPosition and bottomPosition, set a class called "is-active", which adds a background-color of red.
+// 1. replace vh values with margin 200px v
+// 2. have correct state when website is opened (put them in a function) -> updatePositions()
+// 3. research browser mechanics layout/composite/paints/reflows/... 
 
 // cache DOM elements
 var blockNodeList = document.querySelectorAll(".js-block");
@@ -14,79 +10,52 @@ var blockOffsetList = [];
 // Section heights
 var blockHeightList = [];
 
-for (var i = 0; i < blockNodeList.length; i++) {
-	blockOffsetList.push(blockNodeList[i].offsetTop);
-	blockHeightList.push(blockNodeList[i].offsetHeight);
+var windowHeight = 0;
+
+function updatePositions() {
+	windowHeight = window.innerHeight;
+	
+	for (var i = 0; i < blockNodeList.length; i++) {
+		blockOffsetList[i] = blockNodeList[i].offsetTop;
+		blockHeightList[i] = blockNodeList[i].offsetHeight;
+	}
 }
 
-var windowHeight = window.innerHeight;
-
-var blockHeigh
-console.log('blockOffsetList', blockOffsetList);
-console.log('blockHeightList', blockHeightList);
-
-
-
-/*var intro = blockNodeList[0];
-var about = blockNodeList[1];
-var gallery = blockNodeList[2];
-var contact = blockNodeList[3];
-
-var intorPos = intro.offsetTop;
-var aboutPos = about.offsetTop;
-var galleryPos = gallery.offsetTop;
-var contactPos = contact.offsetTop; //don't need them */
-
-// var menuLiNodeList = document.querySelectorAll(".menu li"); //temp off
-
-// function menuRemoveClass() {
-// 	for (var i = 1; i < menuLiNodeList.length; i++) {
-// 		menuLiNodeList[i-1].classList.remove("current_item");
-// 		blockNodeList[i].classList.remove("current_page");
-// 	}
-// }
-
-window.addEventListener("scroll", function() {
-
+function detectPage() {
 	var scrollTop = document.body.scrollTop;
 
 	var viewCenter = scrollTop + windowHeight * .5;
 	var topRange = viewCenter - 100;
 	var bottomRange = viewCenter + 100;
-	
-	// var halfWindowHeight = windowHeight / 2;
-
-	// var midPos = topPosition + halfWindowHeight;
-
-	// console.log("window.scrollTop", document.body.scrollTop);
-
-	// console.log(windowHeight, halfWindowHeight);
-	// console.log(bottomPosition);
-	// console.log(midPos);
-	// console.log(aboutPos);
-	// console.log("contactPos: " + contactPos);
 
 	for (var i = 0, l = blockNodeList.length; i < l; i++) {
 		
-/*		if (topRange < blockOffsetList[i] && bottomRange > blockOffsetList[i]) {
-			blockNodeList[i].classList.add('is-active');
-			
-			// menuRemoveClass();
-			// menuLiNodeList[i-1].classList.add("current_item");
-			// blockNodeList[i].classList.add("is-active");
-		
-		} else {
-			blockNodeList[i].classList.remove('is-active');
-		}*/
 		var blockbottom = blockHeightList[i] + blockOffsetList[i];
 
-		if (blockOffsetList[i] < bottomRange && blockbottom > topRange) {
+		var bottomBelowTop = blockbottom > topRange;
+
+		var topAboveBottom = blockOffsetList[i] < bottomRange;
+
+		if (bottomBelowTop && topAboveBottom) {
 			blockNodeList[i].classList.add('is-active');
 		} else {
 			blockNodeList[i].classList.remove('is-active');
 		}
 
 	}
+}
 
+updatePositions();
+
+detectPage();
+
+window.addEventListener("resize", function() {
+	console.log("resized!!");
+
+	updatePositions();
+
+	console.log(blockOffsetList);
 });
+
+window.addEventListener("scroll", detectPage);
 
