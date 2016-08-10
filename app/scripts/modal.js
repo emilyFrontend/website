24321e372807d1c3db50802d2ef407f8774aee99
Emily Wu <embrilliant@hotmail.com>
2016-08-10 15:26:32 +0200

    add improvements to modal

--- app/scripts/modal.js
+++ app/scripts/modal.js
@@ -1,17 +1,38 @@
 
 var imgNumber = 1;
 
-var prevButton = document.querySelector(".previous");
-var nextButton = document.querySelector(".next");
-var closeButton = document.querySelector(".modal-close");
+var modal = document.querySelector(".modal");
+var screenOverlay = document.querySelector(".screen-overlay");
 
-var imgNode = document.querySelector(".modal img");
-var captionNode = document.querySelector(".modal figcaption"); 
+var prevButton = modal.querySelector(".previous");
+var nextButton = modal.querySelector(".next");
+var closeButton = modal.querySelector(".modal-close");
+
+var imgNode = modal.querySelector("img");
+var captionNode = modal.querySelector("figcaption"); 
 
 var gallery = document.querySelector(".js-gallery");
-var modal = document.querySelector(".modal");
 
 var figCaptions = gallery.querySelectorAll("figcaption");
+
+var numOfItems = gallery.querySelectorAll("figure").length;
+
+var closes = document.querySelectorAll(".js-modal-close");
+
+function showModal() {
+	modal.classList.add("modal-show");
+	screenOverlay.classList.add("modal-show");
+}
+
+function hideModal() {
+	modal.classList.remove("modal-show");
+	screenOverlay.classList.remove("modal-show");
+}
+
+function modalContent(imgNumber) {
+	imgNode.src = "images/gallery/modal/" + imgNumber + ".jpg";
+	captionNode.textContent = figCaptions[imgNumber-1].textContent;
+}
 
 gallery.addEventListener("click", function(e) {
 
@@ -20,13 +41,11 @@ gallery.addEventListener("click", function(e) {
 	showModal();
 	modalContent(imgNumber);
 
-	// console.log("cur: ", e.currentTarget, "target: ", e.target, "this: ", this, imgNumber);
-
 });
 
 nextButton.addEventListener("click", function() {
 
-	if (imgNumber < 8) {
+	if (imgNumber < numOfItems) {
 		imgNumber++ ;
 	} else {
 		imgNumber = 1;
@@ -41,26 +60,25 @@ prevButton.addEventListener("click", function() {
 	if (imgNumber > 1) {
 		imgNumber-- ;
 	} else {
-		imgNumber = 8;
+		imgNumber = numOfItems;
 	}
 
 	modalContent(imgNumber);
 
 });
 
+for (var i = 0; i < closes.length; i++) {
+	closes[i].addEventListener("click", function() {
+    hideModal();
+  });
+}
+
+/*
 closeButton.addEventListener("click", function() {
 	hideModal();
 });
 
-function showModal() {
-	modal.classList.add("modal-show");
-}
-
-function hideModal() {
-	modal.classList.remove("modal-show");
-}
-
-function modalContent(imgNumber) {
-	imgNode.src = "images/gallery/modal/" + imgNumber + ".jpg";
-	captionNode.textContent = figCaptions[imgNumber-1].textContent;
-}
+screenOverlay.addEventListener("click", function() {
+	hideModal();
+});
+*/
