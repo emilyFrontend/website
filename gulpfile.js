46958a703665a60a32268f836f884d6d4f090103
Emily Wu <embrilliant@hotmail.com>
2016-08-17 18:01:41 +0200

    js scss html and browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var ext_replace = require('gulp-ext-replace');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 var livereload = require('gulp-livereload');
 var browserSync = require('browser-sync').create();
 
@@ -20,9 +21,10 @@ gulp.task("html", function() {
   return gulp.src(
   	"src/index.html"
   	)
-    // .pipe(htmlreplace({
-    //   "css": "styles/style.min.css"
-    // }))
+    .pipe(htmlreplace({
+      // "css": "styles/style.min.css",
+      "js": "scripts/app.js"
+    }))
     .pipe(gulp.dest("build/"))
     .pipe(browserSync.stream());
 });
@@ -30,7 +32,6 @@ gulp.task("html", function() {
 gulp.task("copy", function() {
 	return gulp.src([
 			"src/images/**/*",
-			"src/scripts/**/*",
 			"src/fonts/*"
 		], 
 		{
@@ -48,6 +49,13 @@ gulp.task("sass", function() {
     .pipe(browserSync.stream());
 });
 
+gulp.task("js", function() {
+	return gulp.src("src/scripts/*.js")
+		.pipe(concat("app.js"))
+		.pipe(gulp.dest("build/scripts/"))
+		.pipe(browserSync.stream());
+});
+
 gulp.task("watch", function () {
 	livereload.listen();
   gulp.watch("src/styles/*.scss", ["sass"]);
@@ -55,15 +63,17 @@ gulp.task("watch", function () {
 });
 
 gulp.task("build", function() {
-	runSequence("clean", ["sass", "html", "copy"]);
+	runSequence("clean", ["sass", "html", "js", "copy"]);
 });
 
 gulp.task("serve", ["build"], function() {
 
   browserSync.init({
-      server: "./build"
+      server: "./build",
+      port: 1234
   });
 
   gulp.watch("src/styles/*.scss", ["sass"]);
   gulp.watch("src/index.html", ["html"]);
+  gulp.watch("src/scripts/*.js", ["js"]);
 });
